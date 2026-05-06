46a47aad11356273ebe5e6fd347bb40abbed4388
spacedrag7 <debadrita7102007@gmail.com>
2026-05-06 12:58:40 +0530

    Create Config.java

---
+++ bootstrap/src/main/java/io/jenkins/jenkinsfile/runner/bootstrap/Config.java
@@ -0,0 +1,8 @@
+package io.jenkins.jenkinsfile.runner.bootstrap;
+
+public class Config {
+  public String pluginsDir;
+  public String war;
+  public String jenkinsfile;
+  public String runWorkspace;
+}
